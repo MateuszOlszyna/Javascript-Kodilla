2ec99f74e75a6c919b01809fe37f559a8866deba
MateuszOlszyna <mateusz.olszyna@gmail.com>
2017-03-26 21:55:32 +0200

    variable

---
+++ Zadanie-8-4/scripts.js
@@ -0,0 +1,9 @@
+// plik scripts.js
+
+// triangleArea calculation
+
+var a = prompt ("Type value a"),
+	h = prompt ("Type value h"),
+	triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);	
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
