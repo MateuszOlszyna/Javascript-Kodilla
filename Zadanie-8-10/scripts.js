671bb660019c5580e9ea02686238aeef93e18f0d
MateuszOlszyna <mateusz.olszyna@gmail.com>
2017-04-03 19:41:37 +0200

    add function

--- Zadanie-8-10/scripts.js
+++ Zadanie-8-10/scripts.js
@@ -1,11 +1,10 @@
 // plik scripts.js
 
 var buttons = document.getElementsByClassName ("button");
-console.log(buttons)
 
-//	function alertButtons (buttons) {
-//		for (var i = 0; i < buttons.length; i++) {
-//			alert(buttons.innerHTML)
+	function alertButtons (buttons) {
+	for (var i = 0; i < buttons.length; i++) {
+		alert(buttons.innerHTML)
 
-//		}
-//	}
+	}
+	}
