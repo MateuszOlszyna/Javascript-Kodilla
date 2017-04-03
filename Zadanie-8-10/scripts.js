2ad7aeb15cf0d12766bc9592127721c710d27d10
MateuszOlszyna <mateusz.olszyna@gmail.com>
2017-04-03 19:39:57 +0200

    JS DOM

---
+++ Zadanie-8-10/scripts.js
@@ -0,0 +1,11 @@
+// plik scripts.js
+
+var buttons = document.getElementsByClassName ("button");
+console.log(buttons)
+
+//	function alertButtons (buttons) {
+//		for (var i = 0; i < buttons.length; i++) {
+//			alert(buttons.innerHTML)
+
+//		}
+//	}
